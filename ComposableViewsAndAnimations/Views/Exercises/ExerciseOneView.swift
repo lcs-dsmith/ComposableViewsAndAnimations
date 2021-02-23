5f38ecb0f81401de11a97cf086e947493e2ca634
lcs-dsmith <dsmith@lcs.on.ca>
2021-02-23 15:01:21 -0500

    Text moves, changes size and colour

--- ComposableViewsAndAnimations/Views/Exercises/ExerciseOneView.swift
+++ ComposableViewsAndAnimations/Views/Exercises/ExerciseOneView.swift
@@ -20,7 +20,12 @@ struct ExerciseOneView: View {
 
     // Whether to apply the animation
     @State private var useAnimation = false
+    
+    @State private var hue: Color = .red
 
+    @State private var scaleFactor: CGFloat = 1
+    
+    @State private var offset: CGFloat = 1
     // MARK: Computed properties
 
     // List all fonts available
@@ -42,8 +47,38 @@ struct ExerciseOneView: View {
                 
                 Text(typeFace)
                     .font(.custom(typeFace, size: 30.0))
-                
+                    .foregroundColor(hue)
+                    .scaleEffect(scaleFactor)
+                    .offset(x: /*@START_MENU_TOKEN@*/10.0/*@END_MENU_TOKEN@*/, y: offset)
+                    .onTapGesture {
+                        if scaleFactor > 0.2 {
+                            // Reduce the size of the circle by a tenth
+                            scaleFactor -= 0.1
+                        } else {
+                            // Make sure the button doesn't entirely disappear
+                            scaleFactor = 1
+                        }
+                        if scaleFactor > 0.2 {
+                            // Reduce the size of the circle by a tenth
+                            scaleFactor -= 0.1
+                        } else {
+                            // Make sure the button doesn't entirely disappear
+                            scaleFactor = 1
+                            // Change the color of the view to a random hue
+                            hue = Color(hue: Double.random(in: 1...360) / 360.0,
+                                        saturation: 0.8,
+                                        brightness: 0.8)
+                        }
+                        if offset > -30 {
+                            // Reduce the size of the circle by a tenth
+                             offset -= 10
+                        } else {
+                            // Make sure the button doesn't entirely disappear
+                            offset = 0
+                        }
+                    }
             }
+            
             .navigationTitle("Exercise 1")
             .toolbar {
                 ToolbarItem(placement: .primaryAction) {
